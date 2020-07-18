49ac672ade9d49919b7f6d4b5afe796f24e27a3b
Charlyln <charleshenry.lenue>
2020-07-18 13:43:57 +0200

    create follower routes

---
+++ routes/followers.route.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const followers = express.Router();
+const Follower = require("../models/followers.model");
+
+followers.get("/", async (req, res) => {
+  try {
+    const followers = await Follower.findAll();
+    res.status(200).json(followers);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+followers.post("/", async (req, res) => {
+  const { UserUuid, followerId } = req.body;
+  try {
+    const followers = await Follower.create({
+      UserUuid,
+      followerId,
+    });
+    res.status(201).json(followers);
+  } catch (err) {
+    res.status(422).json(err);
+  }
+});
+
+followers.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Follower.destroy({ where: { id } });
+    res.status(204).send("Votre follower a été supprimé");
+  } catch (err) {
+    res.status(422).json(err);
+  }
+});
+
+module.exports = followers;

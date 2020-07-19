8ec76306ca26d1a38d2d412e8b20bad33561a7fb
Charlyln <charleshenry.lenue>
2020-07-19 17:06:59 +0200

    add post associations

--- models/association.js
+++ models/association.js
@@ -2,11 +2,12 @@ const User = require("./user.model");
 const Message = require("./message.model");
 const Likes = require("./likes.model");
 const Followers = require("./followers.model");
+const Post = require("./post.model");
 
 User.hasMany(Message, { foreignKey: { allowNull: false } });
 Message.belongsTo(User);
 
-Message.hasMany(Likes, { foreignKey: { allowNull: false } });
+Message.hasMany(Likes, { foreignKey: { allowNull: true } });
 Likes.belongsTo(Message);
 
 User.hasMany(Likes, { foreignKey: { allowNull: false } });
@@ -14,3 +15,9 @@ Likes.belongsTo(User);
 
 User.hasMany(Followers, { foreignKey: { allowNull: false } });
 Followers.belongsTo(User);
+
+User.hasMany(Post, { foreignKey: { allowNull: false } });
+Post.belongsTo(User);
+
+Post.hasMany(Likes, { foreignKey: { allowNull: true } });
+Likes.belongsTo(Post);
